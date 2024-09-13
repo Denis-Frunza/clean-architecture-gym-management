9c2eed65fa725c32aafd20e8ef298ac961c368fb
Denis-Frunza <46573296+Denis-Frunza@users.noreply.github.com>
2024-09-13 08:56:15 +0300

    add subscription methods (#2)
    Co-authored-by: denis.frunza <denisfrunza.93@gmail.com>

--- src/application/interfaces/repositories.py
+++ src/application/interfaces/repositories.py
@@ -1,14 +1,15 @@
+from abc import ABC, abstractmethod
 import typing as t
 
-from abc import ABC, abstractmethod
 from domain.subscriptions.entities import Subscription
 
 
 class SubscriptionRepository(ABC):
     @abstractmethod
-    def create(self, subscription: Subscription) -> Subscription:
-        ...
+    def create(self, subscription: Subscription) -> Subscription: ...
 
     @abstractmethod
-    def find_by_id(self, subscription_id: int) -> t.Optional[Subscription]:
-        ...
+    def find_by_id(self, subscription_id: int) -> t.Optional[Subscription]: ...
+
+    @abstractmethod
+    def delete(self, subscription_id: int): ...
--- src/application/services/subscription_service.py
+++ src/application/services/subscription_service.py
@@ -1,4 +1,4 @@
-from application.use_cases.create_subscription import CreateSubscriptionUseCase
+from application.use_cases.create_subscription import CreateSubscriptionUseCase, DeleteSubscriptionUseCase
 from domain.subscriptions.entities import Subscription, SubscriptionType
 from infrastructure.repositories.subscription_repository import SubscriptionRepository
 
@@ -8,8 +8,23 @@ class SubscriptionService:
 
         self.subscription_repository = subscription_repository
 
-    def create_subscription(self, id: int, user_id: int, plan: SubscriptionType) -> Subscription:
-
-        create_subscription_use_case = CreateSubscriptionUseCase(self.subscription_repository)
-
+    def create_subscription(
+        self, id: int, user_id: int, plan: SubscriptionType
+    ) -> Subscription:
+        create_subscription_use_case = CreateSubscriptionUseCase(
+            self.subscription_repository
+        )
         return create_subscription_use_case.execute(id=id, user_id=user_id, plan=plan)
+
+    def get_subscription(self, subscription_id: int) -> Subscription:
+        subscription = self.subscription_repository.find_by_id(subscription_id)
+
+        if not subscription:
+            raise Exception("Subscription not found")
+        return subscription
+
+    def delete_subscription(self, subscription_id: int):
+        delete_subscription_use_case = DeleteSubscriptionUseCase(
+            self.subscription_repository
+        )
+        delete_subscription_use_case.execute(subscription_id)
